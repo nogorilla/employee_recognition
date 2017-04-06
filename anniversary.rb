8456305cd09c2420db747171aa07a4fe669ee9e4
Gabe Smith <gabriel.smith01@gmail.com>
2017-04-05 20:19:53 -0400

    read from CSV; basic json output

--- anniversary.rb
+++ anniversary.rb
@@ -1,13 +1,15 @@
 #!/usr/bin/ruby
 
 require 'date'
+require 'CSV'
+require 'json'
 
 inputs = ARGV
 file = inputs[0]
 date = inputs[1].nil? ? Time.now.strftime("%Y-%d-%m") : inputs[1]
 
-## Error handling
 
+## Error handling
 ### Invalid file
 unless File.file?(file)
   puts 'File invalid. Enter full file path'
@@ -23,4 +25,36 @@ else
   date = Date.parse(date)
 end
 
-puts "file: #{file}, date: #{date}"
+def employee_json(employee, milestones)
+ {
+    supervisor_id: employee[:id],
+    upcoming_milestones: milestones
+  }
+end
+
+employees = []
+supervisors = []
+
+CSV.foreach(file, headers: true) do |line|
+  employees.push({
+    id: line['employee_id'],
+    first_name: line['first_name'],
+    last_name: line['last_name'],
+    hire_date: line['hire_date'],
+    supervisor_id: line['supervisor_id']
+  })
+  supervisors.push(line['supervisor_id']) unless supervisors.include? line['supervisor_id']
+end
+
+
+employees.each do |employee|
+  milestones = []
+  if supervisors.include? employee[:id]
+    milestones = ['test']
+  end
+  puts employee_json(employee, milestones)
+end
+
+# employees.each do |employee|
+#   puts employees
+# end
